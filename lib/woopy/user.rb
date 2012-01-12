a49b7dbcaecb609552b77167f6c5d56c8c8eaa5d
Paul Kalupnieks <paul@bigbangtechnology.com>
2012-01-12 13:37:35 -0500

    Created user resource

--- lib/woopy/user.rb
+++ lib/woopy/user.rb
@@ -1,10 +1,5 @@
 module Woopy
   class User < Resource
 
-    def initialize(options)
-      @token = options[:token]
-      Resource.headers['X-WoopleToken'] = @token
-    end
-    
   end
 end
--- spec/woopy/user_spec.rb
+++ spec/woopy/user_spec.rb
@@ -7,58 +7,40 @@ describe Woopy::User do
     @user_id = 1
     @name = "Test User"
     @email = "test@example.com"
-    @image_url = "/images/thumb/missing.png"
+    Woopy(token: @token)
   end
 
   describe "#new" do
-    before do
-      path = '/services/v1/users'
-      request_headers = {"Accept" => "application/json", "X-WoopleToken" => @token }
-      response_body = {id: @user_id, name: @name, email: @email, image_url: @image_url}.to_json
-      response_headers = { "Location" => "#{path}/#{@user_id}.json" }
-      ActiveResource::HttpMock.respond_to do |mock|
-        mock.post( path, request_headers, response_body, 200, response_headers )
-      end
+    subject { Woopy::User.new(name: @name, email: @email) }
+
+    it 'sets a token' do
+      Woopy::User.headers["X-WoopleToken"].should eql(@token)
     end
 
-    subject { Woopy::User.new(token: @token, name: @name, email: @email) }
-
-    it { should be }
+    it { should be_kind_of Woopy::User }
     
   end
 
   describe "#save" do
 
     before do
-      path = '/services/v1/users'
-      request_headers = {"Accept" => "application/json", "X-WoopleToken" => @token }
-      response_body = {id: @user_id, name: @name, email: @email, image_url: @image_url}.to_json
-      response_headers = { "Location" => "#{path}/#{@user_id}.json" }
+      path = '/services/v1/users.json'
+      request_headers = {"Content-Type" => "application/json", "X-WoopleToken" => @token }
+      response_body = {user: { id: @user_id, name: @name, email: @email }}.to_json
       ActiveResource::HttpMock.respond_to do |mock|
-        mock.post( path, request_headers, response_body, 200, response_headers )
+        mock.post( path, request_headers, response_body)
       end
-      @user = Woopy::User.new(token: @token, name: @name, email: @email) 
     end
 
-    subject { @user.save }
+    context "valid name and email" do
+      before do
+        @user = Woopy::User.new(name: @name, email: @email) 
+      end
+      it 'saves correctly' do
+        @user.save.should be_true
+      end
+    end
 
-    it { should be }
-
-    # its :id do 
-    #   should be_eql(@user_id)
-    # end
-
-    # its :name do
-    #   should be_eql(@name)
-    # end
-
-    # its :email do
-    #   should be_eql(@email)
-    # end
-
-    # its :image_url do
-    #   should be 
-    # end
   end
 
 end
