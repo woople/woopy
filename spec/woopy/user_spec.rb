daf48e5bb0a88c0b64cf5720df9b6e8e27e6c5b1
Paul Kalupnieks <paul@bigbangtechnology.com>
2012-01-12 14:53:03 -0500

    Refactored user spec

--- spec/woopy/user_spec.rb
+++ spec/woopy/user_spec.rb
@@ -4,25 +4,20 @@ describe Woopy::User do
 
   before do 
     @token = 'foo'
-    @user_id = 1
-    @name = "Test User"
-    @email = "test@example.com"
     Woopy(token: @token)
   end
 
   describe "#save" do
 
     before do
-      path = '/services/v1/users.json'
-      response_body = {user: { id: @user_id, name: @name, email: @email }}.to_json
       ActiveResource::HttpMock.respond_to do |mock|
-        mock.post( path, request_headers(@token), response_body)
+        mock.post( '/services/v1/users.json', request_headers(@token), user_response)
       end
     end
 
     context "valid name and email" do
       before do
-        @user = Woopy::User.new(name: @name, email: @email) 
+        @user = Woopy::User.new(user_attributes) 
       end
       it 'saves correctly' do
         @user.save.should be_true
