d0503c51cb3a66c9759b59760fba4a80c83f51fc
Joannou Ng <joannou@bigbangtechnology.com>
2012-01-16 12:17:38 -0500

    Added invalid user attributes test
    [#23294237]

--- spec/woopy/user_spec.rb
+++ spec/woopy/user_spec.rb
@@ -1,29 +1,37 @@
 require 'spec_helper'
 
 describe Woopy::User do
-
   before do 
     @token = 'foo'
     Woopy(token: @token)
   end
 
   describe "#save" do
-
-    before do
-      ActiveResource::HttpMock.respond_to do |mock|
-        mock.post( '/services/v1/users.json', request_headers(@token), user_response)
-      end
-    end
-
     context "valid name and email" do
       before do
+        ActiveResource::HttpMock.respond_to do |mock|
+          mock.post( '/services/v1/users.json', request_headers(@token), user_response)
+        end
         @user = Woopy::User.new(user_attributes) 
       end
+
       it 'saves correctly' do
         @user.save.should be_true
       end
     end
-
+    
+    context "invalid attributes" do
+      before do
+        ActiveResource::HttpMock.respond_to do |mock|
+          mock.post('/services/v1/users.json', request_headers(@token), { errors: ["Name can't be blank"] }.to_json, 422)
+        end
+        @user = Woopy::User.new(user_attributes) 
+      end
+      
+      it "should not save, with errors" do
+        @user.save.should be_false
+        @user.errors.full_messages.should include("Name can't be blank")
+      end
+    end
   end
-
 end
