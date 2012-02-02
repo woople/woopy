932cf3adde4e57154b0968a2a26470f251864c6b
David Wright <davidwright@gmail.com>
2012-02-02 14:41:27 -0500

    Added role interface for the employment methods

--- lib/woopy/account.rb
+++ lib/woopy/account.rb
@@ -1,12 +1,12 @@
 module Woopy
   class Account < Resource
 
-    def employ(user)
-      Employment.create(account_id: self.id, user_id: user.id)
+    def employ(user, role = nil)
+      Employment.create(account_id: self.id, user_id: user.id, role: role)
     end
 
-    def make_owner(user)
-      Ownership.create(account_id: self.id, user_id: user.id)
+    def make_owner(user, role = nil)
+      Ownership.create(account_id: self.id, user_id: user.id, role: role)
     end
 
   end
