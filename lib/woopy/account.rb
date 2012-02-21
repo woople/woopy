3f4780f8670105f447818ebc614600102ef4cfa9
Adam Doeler <adam@adamdoeler.com>
2012-02-21 10:44:47 -0500

    Removed code to lookup all employments as it is never used.
    [#22734017]

--- lib/woopy/account.rb
+++ lib/woopy/account.rb
@@ -12,10 +12,6 @@ module Woopy
       Ownership.create(account_id: self.id, user_id: user.id, role: role)
     end
 
-    def employments
-      Employment.find(:all, params: { account_id: self.id })
-    end
-
     def find_employment(user)
       Employment.find(:first, params: { account_id: self.id, user_id: user.id})
     end
